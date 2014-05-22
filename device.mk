153dc791042ca35f5cfd9f3d1713b4dafeb8af2f
raymanfx <raymanfx@gmail.com>
2014-05-22 11:19:03 +0200

    Find7: remove unneeded sysprop values

--- device.mk
+++ device.mk
@@ -324,6 +324,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.qc.sdk.sensors.gestures=true
 
+# GPS Configuration
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qc.sdk.izat.premium_enabled=1 \
+    ro.qc.sdk.izat.service_mask=0x5 \
+    persist.gps.qc_nlp_in_use=0 \
+    ro.gps.agps_provider=1
+
 # Networks
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.ril_class=QualcommMSIM42RIL
