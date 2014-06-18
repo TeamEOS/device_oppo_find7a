11b4927d5b6fb89f49a8bc95a8037a69723b4ad8
raymanfx <raymanfx@gmail.com>
2014-06-18 09:03:58 +0200

    Find7: Our NFC chip does not support Host Card Emulation

--- device.mk
+++ device.mk
@@ -124,8 +124,7 @@ PRODUCT_COPY_FILES += \
 
 # NFC access control + feature files
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml
 
 PRODUCT_COPY_FILES += \
     device/oppo/find7/configs/thermald-8974.conf:system/etc/thermald-8974.conf \
