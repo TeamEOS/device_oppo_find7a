e8fdc4bb48c6ec7dec532825dc786f5dafd7fa78
raymanfx <raymanfx@gmail.com>
2014-07-04 13:05:09 +0200

    Find7: no need to bloat up dtimage twice

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,7 +42,6 @@ BOARD_KERNEL_SEPARATED_DT := true
 
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.selinux=permissive androidboot.hardware=find7 user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01E00000
-BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 
 # Flags
 COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
